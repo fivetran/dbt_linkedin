832084b493979d9b53d7b42ce7917dae62e7ac80
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-10-30 12:03:59 -0700

    docs and tests

--- integration_tests/tests/consistency/consistency_campaign_group_report.sql
+++ integration_tests/tests/consistency/consistency_campaign_group_report.sql
@@ -8,7 +8,9 @@ with prod as (
         account_id,
         sum(clicks) as clicks, 
         sum(impressions) as impressions,
-        sum(cost) as cost
+        sum(cost) as cost,
+        sum(total_conversions) as total_conversions,
+        sum(conversion_value_in_local_currency) as conversion_value_in_local_currency
     from {{ target.schema }}_linkedin_prod.linkedin_ads__campaign_group_report
     group by 1
 ),
@@ -18,7 +20,9 @@ dev as (
         account_id,
         sum(clicks) as clicks, 
         sum(impressions) as impressions,
-        sum(cost) as cost
+        sum(cost) as cost,
+        sum(total_conversions) as total_conversions,
+        sum(conversion_value_in_local_currency) as conversion_value_in_local_currency
     from {{ target.schema }}_linkedin_dev.linkedin_ads__campaign_group_report
     group by 1
 ),
@@ -31,7 +35,11 @@ final as (
         prod.impressions as prod_impressions,
         dev.impressions as dev_impressions,
         prod.cost as prod_cost,
-        dev.cost as dev_cost
+        dev.cost as dev_cost,
+        prod.total_conversions as prod_total_conversions,
+        dev.total_conversions as dev_total_conversions,
+        prod.conversion_value_in_local_currency as prod_conversion_value_in_local_currency,
+        dev.conversion_value_in_local_currency as dev_conversion_value_in_local_currency
     from prod
     full outer join dev 
         on dev.account_id = prod.account_id
@@ -43,3 +51,5 @@ where
     abs(prod_clicks - dev_clicks) >= .01
     or abs(prod_impressions - dev_impressions) >= .01
     or abs(prod_cost - dev_cost) >= .01
+    or abs(prod_total_conversions - dev_total_conversions) >= .01
+    or abs(prod_conversion_value_in_local_currency - dev_conversion_value_in_local_currency) >= .01
